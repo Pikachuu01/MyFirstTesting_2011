d74897d2f6ff437230de6272bd65837851a1e4fe
Pika_Pika <roman.sabirov01@gmail.com>
2021-12-28 18:10:19 +0300

    Finished
    finished

---
+++ Homework_3.1/Homework_3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длинну столбца ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите длинну строки ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] Arr =new int[n, m];
+
+            Random rn = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Arr[i, j] = rn.Next(1,15);
+                    Console.Write("{0}\t", Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("элементы двумерного массива по диагонали:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (i == j) 
+                    Console.WriteLine(Arr[i, j]+ " ");
+                }
+            }
+        }
+    }
+}
---
+++ Homework_3.2/Homework_3.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Homework_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //«Телефонный справочник»
+
+            string[,] Arr =
+            {
+                {
+                "Иванов",
+                "Петров",
+                "Сидоров",
+                "Филиппова",
+                "Егоров"
+                },
+                {
+                "o@outlook.com",
+                "hr6zdl@yandex.ru",
+                "kaft93x@outlook.com",
+                "dcu@yandex.ru",
+                "19dn@outlook.com"
+                }
+            };
+            Console.Write("Введите фамилию: ");
+            string lastName = Console.ReadLine();
+            bool isFound = false;
+
+            for (int i = 0; i < Arr.Rank; i++)
+            {
+                for (int j = 0; j < Arr.Rank; j++)
+                {
+                    if (Arr[0 , j] == lastName && !isFound) 
+                    {
+                        Console.WriteLine($"человек с фамилией {lastName} имеет почту: {Arr[1, j]}");
+                        isFound = true;
+                        break;
+                    }
+
+
+                }
+                Console.WriteLine();
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("Такого человека нет в списке :(");
+            }
+
+        }
+    }
+}
